129137e706785b07b1ac62e1ca4fea38326d66a8
Bala Krishna <balumakineni8008@gmail.com>
2023-04-10 19:55:14 +0530

    added database connection..

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
-const homeStartingContent = "This is my daily journal or blog. Anyone can post a journal or blog.";
+const homeStartingContent = "This is my daily journal or blog. Anyone can post a journal or blog in this blogpost.";
 const aboutContent = "I am currently pursuing my Bachelor's in Computer Science stream. I am in penultimate year of my bachelor's, currently looking for an internship.";
-const contactContent = "";
+const contactContent = "whatsapp,instagram";
 
 const app = express();
 
@@ -16,13 +17,22 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb+srv://balu:y20cs108@cluster0.iwk3gyj.mongodb.net/blogDB", {useNewUrlParser: true});
 
-app.get("/",(req,res)=>{
-  res.render("home",{
+const postSchema = {
+  title: String,
+  content: String 
+};
+const Post = mongoose.model("Post", postSchema);
+
+
+app.get("/",async (req,res)=>{
+  const posts = await Post.find()
+  res.render("home", {
     homeContent: homeStartingContent,
     posts: posts
   });
+
 })
 
 app.get("/about",(req,res)=>{
@@ -38,28 +48,28 @@ app.get("/compose",(req,res)=>{
 })
 
 app.post("/compose",(req,res)=>{
-  const post={
+  const post = new Post ({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
+  });
+  post.save();
   res.redirect("/");
 })
 
-app.get("/posts/:postName",(req,res)=>{
-  const requestedTitle=_.lowerCase(req.params.postName);
-  posts.forEach((post)=>{
-    const storedTitle = _.lowerCase(post.title)
-    if(requestedTitle == storedTitle ){
-      res.render("post",{
-        title: post.title,
-        content:post.content
-      });
-    }
-  })
+app.get("/posts/:postId",async (req,res)=>{
+  const requestedPostId = req.params.postId;
+  const post = await Post.findOne({_id: requestedPostId});
+  res.render("post",{
+    id: post._id,
+    title: post.title,
+    content: post.content
+  });
 })
 
-
+app.post("/:postId",async (req,res)=>{
+  await Post.deleteOne({_id:req.params.postId});
+  res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
