f78505f718222ee4fdbaf8457b4372cc9a186698
Bala Krishna <balumakineni8008@gmail.com>
2023-04-11 17:33:33 +0530

    added package files.

--- app.js
+++ app.js
@@ -71,6 +71,6 @@ app.post("/:postId",async (req,res)=>{
   res.redirect("/");
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
